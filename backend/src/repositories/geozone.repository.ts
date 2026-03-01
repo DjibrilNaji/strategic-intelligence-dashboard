bcae0781a87de531ab938d3ad562146b6111b0a2
Djibril <najidjibril@gmail.com>
2026-03-01 15:51:38 +0100

    feat: add geozone repositorie

---
+++ backend/src/repositories/geozone.repository.ts
@@ -0,0 +1,13 @@
+import { Kysely } from "kysely";
+
+import { Database } from "@/db";
+import { NewGeoZone } from "@/types/db";
+
+export async function insertGeoZone(db: Kysely<Database>, data: NewGeoZone) {
+  return db
+    .insertInto("geoZones")
+    .values(data)
+    .onConflict((oc) => oc.column("country").doNothing())
+    .returningAll()
+    .executeTakeFirst();
+}
